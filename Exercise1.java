b236b2c04be282c2581dd8ba581d250840897364
Vogo Coder <93537821+vogo-coder@users.noreply.github.com>
2021-11-01 16:51:23 +0100

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,17 @@
+// While loop example 
+class Exercise1{ 
+
+    public static void main(String args[]) 
+
+    { 
+
+        while (True) { 
+
+            System.out.println("This is java");
+
+        } 
+
+    } 
+
+} 
+
